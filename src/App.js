eaaae527fc87c0c311d0666b77f87550cf5f898d
Alexandr <fomin02@inbox.ru>
2023-04-26 16:17:59 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Block'>
+        <p className="Pre-H">API</p>
+        <h1 className="H">Offset Your Carbon Footprint in Real Time</h1>
+        <p className="Description">Cloverly's powerful API calculates the impact of common carbon-intensive activities in real time and uses verified, quality carbon offset projects to neutralize them. Ready to get started? Create an account to integrate the Cloverly API for free, or contact us for custom solutions.</p>
+        <div className="Btn-section">
+          <button className="Btn-primary">Get Started for Free</button>
+          <button className="Btn-secondary" >Get Started for Free</button>
+        </div>
+      </div>
+  </div>
+  );
+}
+
+export default App;
